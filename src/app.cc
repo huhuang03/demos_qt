2e1347def0f7063e7581573def31a6f2dd8c6c31
york <york@duomai.com>
2021-10-12 22:57:50 +0800

    up

--- src/app.cc
+++ src/app.cc
@@ -5,19 +5,24 @@
 #include "app.h"
 #include <iostream>
 #include "./demos/clock/clock_view.h"
+#include "./demos/img_viewer/img_viewer.h"
 
 App::App(QWidget *parent): QMainWindow(parent) {
     this->resize(800, 600);
 
     this->listWidget = new QListWidget(this);
     listWidget->addItem("Clock View");
+    listWidget->addItem("Image Viewer");
     connect(listWidget, &QListWidget::itemClicked, this, &App::onItemClick);
+    setCentralWidget(this->listWidget);
 }
 
 void App::onItemClick(QListWidgetItem *item) {
     if (this->listWidget->item(0) == item) {
-        std::cout << "onItemClick 0 called" << std::endl;
         auto clockView = new ClockView();
         clockView->show();
+    } else if (this->listWidget->item(1) == item) {
+        auto imgViewer = new ImgViewer();
+        imgViewer->show();
     }
 }
---
+++ src/demos/img_viewer/img_viewer.cc
@@ -0,0 +1,244 @@
+//
+// Created by huhua on 2021/10/12.
+//
+
+#include "img_viewer.h"
+
+#include <QGuiApplication>
+#include <QScreen>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QImageReader>
+#include <QImageWriter>
+#include <QMessageBox>
+#include <QMenuBar>
+#include <QApplication>
+#include <iostream>
+
+ImgViewer::ImgViewer(QWidget *parent)
+    : QMainWindow(parent)
+    , imageLabel(new QLabel)
+    , scrollArea(new QScrollArea) {
+
+    imageLabel->setBackgroundRole(QPalette::Base);
+    imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    imageLabel->setScaledContents(true);
+
+    scrollArea->setBackgroundRole(QPalette::Dark);
+    scrollArea->setWidget(imageLabel);
+    scrollArea->setVisible(false);
+    setCentralWidget(scrollArea);
+
+    createActions();
+    this->resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
+}
+
+static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+{
+    static bool firstDialog = true;
+
+    if (firstDialog) {
+        firstDialog = false;
+        const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+        dialog.setDirectory(picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+    }
+
+    QStringList mimeTypeFilters;
+    const QByteArrayList supportedMimeTypes = acceptMode == QFileDialog::AcceptOpen
+                                              ? QImageReader::supportedMimeTypes() : QImageWriter::supportedMimeTypes();
+    for (const QByteArray &mimeTypeName : supportedMimeTypes)
+        mimeTypeFilters.append(mimeTypeName);
+    mimeTypeFilters.sort();
+    dialog.setMimeTypeFilters(mimeTypeFilters);
+    dialog.selectMimeTypeFilter("image/jpeg");
+    dialog.setAcceptMode(acceptMode);
+    if (acceptMode == QFileDialog::AcceptSave)
+        dialog.setDefaultSuffix("jpg");
+}
+
+void ImgViewer::open()
+{
+    QFileDialog dialog(this, tr("Open File"));
+    initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
+
+    while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().constFirst())) {}
+}
+
+bool ImgViewer::loadFile(const QString &fileName) {
+    QImageReader reader(fileName);
+    reader.setAutoTransform(true);
+    const QImage newImage = reader.read();
+    if (newImage.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1: %2")
+                                         .arg(QDir::toNativeSeparators(fileName), reader.errorString()));
+        return false;
+    }
+
+    setImage(newImage);
+
+    scaleFactor = 1.0;
+    scrollArea->setVisible(true);
+    printAct->setEnabled(true);
+    fitToWindowAct->setEnabled(true);
+    updateActions();
+
+    std::cout << "size: " << imageLabel->pixmap()->size().width() << "x" << imageLabel->pixmap()->size().height() << std::endl;
+    if (!fitToWindowAct->isChecked())
+        imageLabel->adjustSize();
+    return true;
+}
+
+void ImgViewer::zoomIn()
+{
+    scaleImage(1.25);
+}
+
+void ImgViewer::zoomOut()
+{
+    scaleImage(0.8);
+}
+
+void ImgViewer::normalSize()
+{
+    imageLabel->adjustSize();
+    scaleFactor = 1.0;
+}
+
+void ImgViewer::fitToWindow()
+{
+    bool fitToWindow = fitToWindowAct->isChecked();
+    scrollArea->setWidgetResizable(fitToWindow);
+    if (!fitToWindow)
+        normalSize();
+    updateActions();
+}
+
+void ImgViewer::about()
+{
+    QMessageBox::about(this, tr("About Image Viewer"),
+                       tr("<p>The <b>Image Viewer</b> example shows how to combine QLabel "
+                          "and QScrollArea to display an image. QLabel is typically used "
+                          "for displaying a text, but it can also display an image. "
+                          "QScrollArea provides a scrolling view around another widget. "
+                          "If the child widget exceeds the size of the frame, QScrollArea "
+                          "automatically provides scroll bars. </p><p>The example "
+                          "demonstrates how QLabel's ability to scale its contents "
+                          "(QLabel::scaledContents), and QScrollArea's ability to "
+                          "automatically resize its contents "
+                          "(QScrollArea::widgetResizable), can be used to implement "
+                          "zooming and scaling features. </p><p>In addition the example "
+                          "shows how to use QPainter to print an image.</p>"));
+}
+
+void ImgViewer::createActions()
+{
+    QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
+
+    QAction *openAct = fileMenu->addAction(tr("&Open..."), this, &ImgViewer::open);
+    openAct->setShortcut(QKeySequence::Open);
+
+    saveAsAct = fileMenu->addAction(tr("&Save As..."), this, &ImgViewer::saveAs);
+    saveAsAct->setEnabled(false);
+
+    printAct = fileMenu->addAction(tr("&Print..."), this, &ImgViewer::print);
+    printAct->setShortcut(QKeySequence::Print);
+    printAct->setEnabled(false);
+
+    fileMenu->addSeparator();
+
+    QAction *exitAct = fileMenu->addAction(tr("E&xit"), this, &QWidget::close);
+    exitAct->setShortcut(tr("Ctrl+Q"));
+
+    QMenu *editMenu = menuBar()->addMenu(tr("&Edit"));
+
+    copyAct = editMenu->addAction(tr("&Copy"), this, &ImgViewer::copy);
+    copyAct->setShortcut(QKeySequence::Copy);
+    copyAct->setEnabled(false);
+
+    QAction *pasteAct = editMenu->addAction(tr("&Paste"), this, &ImgViewer::paste);
+    pasteAct->setShortcut(QKeySequence::Paste);
+
+    QMenu *viewMenu = menuBar()->addMenu(tr("&View"));
+
+    zoomInAct = viewMenu->addAction(tr("Zoom &In (25%)"), this, &ImgViewer::zoomIn);
+    zoomInAct->setShortcut(QKeySequence::ZoomIn);
+    zoomInAct->setEnabled(false);
+
+    zoomOutAct = viewMenu->addAction(tr("Zoom &Out (25%)"), this, &ImgViewer::zoomOut);
+    zoomOutAct->setShortcut(QKeySequence::ZoomOut);
+    zoomOutAct->setEnabled(false);
+
+    normalSizeAct = viewMenu->addAction(tr("&Normal Size"), this, &ImgViewer::normalSize);
+    normalSizeAct->setShortcut(tr("Ctrl+S"));
+    normalSizeAct->setEnabled(false);
+
+    viewMenu->addSeparator();
+
+    fitToWindowAct = viewMenu->addAction(tr("&Fit to Window"), this, &ImgViewer::fitToWindow);
+    fitToWindowAct->setEnabled(false);
+    fitToWindowAct->setCheckable(true);
+    fitToWindowAct->setShortcut(tr("Ctrl+F"));
+
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+
+    helpMenu->addAction(tr("&About"), this, &ImgViewer::about);
+    helpMenu->addAction(tr("About &Qt"), this, &QApplication::aboutQt);
+}
+
+void ImgViewer::updateActions()
+{
+    saveAsAct->setEnabled(!image.isNull());
+    copyAct->setEnabled(!image.isNull());
+    zoomInAct->setEnabled(!fitToWindowAct->isChecked());
+    zoomOutAct->setEnabled(!fitToWindowAct->isChecked());
+    normalSizeAct->setEnabled(!fitToWindowAct->isChecked());
+}
+
+void ImgViewer::scaleImage(double factor)
+{
+    scaleFactor *= factor;
+    imageLabel->resize(scaleFactor * imageLabel->pixmap(Qt::ReturnByValue).size());
+
+    adjustScrollBar(scrollArea->horizontalScrollBar(), factor);
+    adjustScrollBar(scrollArea->verticalScrollBar(), factor);
+
+    zoomInAct->setEnabled(scaleFactor < 3.0);
+    zoomOutAct->setEnabled(scaleFactor > 0.333);
+}
+
+void ImgViewer::adjustScrollBar(QScrollBar *scrollBar, double factor)
+{
+    scrollBar->setValue(int(factor * scrollBar->value()
+                            + ((factor - 1) * scrollBar->pageStep()/2)));
+}
+
+void ImgViewer::setImage(const QImage &newImage) {
+    std::cout << "setImage called" << std::endl;
+    this->image = newImage;
+    imageLabel->setPixmap(QPixmap::fromImage(newImage, Qt::AutoColor));
+}
+
+void ImgViewer::createMenus() {
+
+}
+
+bool ImgViewer::saveFile(const QString &fileName) {
+    return false;
+}
+
+void ImgViewer::saveAs() {
+
+}
+
+void ImgViewer::print() {
+
+}
+
+void ImgViewer::copy() {
+
+}
+
+void ImgViewer::paste() {
+
+}
